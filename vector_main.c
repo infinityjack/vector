c260c6d6296c7d5f3bac784b2d0108529c9f2776
jack_cizon <105433087+infinityjack@users.noreply.github.com>
2023-02-09 01:46:33 +0800

    Update vector_main.c

--- vector_main.c
+++ vector_main.c
@@ -1,6 +1,7 @@
 #include"vector.h"
 
 
+
 vector(int);
 
 vector(float);
@@ -11,10 +12,60 @@ vector(char);
 
 vector(string);
 
+PRIVATE void int_operations();    
+
+PRIVATE void double_operations();
+
+PRIVATE void char_operations();
+
+PRIVATE void string_operations();
+
+PRIVATE void test();
+
+
+
+void main(void) {
+
+    int_operations();    
+
+    double_operations();
+
+    char_operations();
+
+    string_operations();
+
+    test();
+
+    puts("press enter to exit...");
+    getchar();
+}
+
+
+
+
+PRIVATE void test(void)
+{
+    int_vector* i = int_new(3, 1, 2, 3);
+    int_traverse(i);
+
+    char_vector* c = char_new(0);
+    char_traverse(c);
+
+    float_vector* l = float_new(0);
+    float_traverse(l);
+
+    double_vector* f = double_new(3, 1.0, 2.0, 3.0);
+    double_traverse(f);
+
+    
+    string_vector* s = string_new(3, "aa", "bb", "cc");
+    string_traverse(s);
+}
+
 
 PRIVATE void int_operations(void)
 {
-  int_vector* vdecimal = int_new();
+  int_vector* vdecimal = int_new(0);
 
     printf("%ld\n", vdecimal->size);
     int_append(vdecimal, 0);
@@ -35,7 +86,7 @@ PRIVATE void int_operations(void)
 
 PRIVATE void double_operations(void)
 {
-  double_vector* vdouble = double_new();
+  double_vector* vdouble = double_new(0);
 
     double_append(vdouble, 4.0);
     double_append(vdouble, 0.0);
@@ -51,7 +102,7 @@ PRIVATE void double_operations(void)
 
 PRIVATE void char_operations(void)
 {
-    char_vector* vchar = char_new();
+    char_vector* vchar = char_new(0);
 
       char_append(vchar, '\n');
       char_append(vchar, 'C');
@@ -72,7 +123,7 @@ PRIVATE void char_operations(void)
 
 PRIVATE void string_operations(void)
 {
-  string_vector* vstring = string_new();
+  string_vector* vstring = string_new(0);
 
     string_append(vstring, "quotes:\t");
     string_append(vstring, "I love C");
@@ -86,16 +137,4 @@ PRIVATE void string_operations(void)
     string_append(vstring, "-------------------------------------------------------------------------------");
 
     string_traverse(vstring);
-}
-
-
-int main() {
-
-    int_operations();    
-
-    double_operations();
-
-    char_operations();
-
-    string_operations();
 }
